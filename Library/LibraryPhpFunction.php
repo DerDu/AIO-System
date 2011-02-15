0e102c62a5617c4b17074e9d477e023a7e77b855
DerDu <blood-demon@gmx.net>
2011-02-15 10:31:20 +0100

    Add LibraryPhpFunction

---
+++ Library/LibraryPhpFunction.php
@@ -0,0 +1,137 @@
+<?php
+// ---------------------------------------------------------------------------------------
+// FUNCTION LIBRARY
+// Extends (existing/missing) PHP-Functionality
+// ---------------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------------
+// LICENSE (BSD)
+//
+//	Copyright (c) 2011, Gerd Christian Kunze
+//	All rights reserved.
+//
+//	Redistribution and use in source and binary forms, with or without
+//	modification, are permitted provided that the following conditions are
+//	met:
+//
+//		* Redistributions of source code must retain the above copyright
+//		  notice, this list of conditions and the following disclaimer.
+//		* Redistributions in binary form must reproduce the above copyright
+//		  notice, this list of conditions and the following disclaimer in the
+//		  documentation and/or other materials provided with the distribution.
+//		* Neither the name of the Gerd Christian Kunze nor the names of its
+//		  contributors may be used to endorse or promote products derived from
+//		  this software without specific prior written permission.
+//
+//	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+//	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+//	PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+//	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+//	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+//	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+//	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ---------------------------------------------------------------------------------------
+
+// ---------------------------------------------------------------------------------------
+// PHP CHECK : IS_?
+if( ! function_exists('is_num') ){
+	function is_num( $propertyValue ){
+		return ( is_int( $propertyValue ) || is_double( $propertyValue ) );
+	}
+}
+if( ! function_exists('is_box') ){
+	function is_box( $propertyValue ){
+		return ( is_array( $propertyValue ) || is_object( $propertyValue ) );
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// PHP CHECK : IN_?
+if( ! function_exists('in_string') ){
+	function in_string( $propertyHaystack, $propertyNeedle ){
+		if( strpos( $propertyHaystack, $propertyNeedle ) === false ) return false;
+		return true;
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// PHP CONVERT : ?2?
+if( ! function_exists('bin2str') ){
+	function bin2str( $propertyBinary ){
+		$propertyBinary = explode(' ',trim(chunk_split($propertyBinary,8,' ')));
+		$propertyBinary = array_map( 'bindec', $propertyBinary );
+		$propertyBinary = array_map( 'chr', $propertyBinary );
+		return implode($propertyBinary);
+	}
+}
+if( ! function_exists('str2bin') ){
+	function str2bin( $propertyString ){
+		$propertyString = str_split( $propertyString, 1 );
+		$propertyString = array_map( 'ord', $propertyString );
+		$propertyString = array_map( 'decbin', $propertyString );
+		return implode($propertyString);
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// PHP ARRAY : ARRAY_?
+if( ! function_exists('array_peek') ){
+	function array_peek( array &$referenceArray ){
+		$peekArray = array_pop( $referenceArray );
+		array_push( $referenceArray, $peekArray );
+		return $peekArray;
+	}
+}
+if (!function_exists('array_intersect_key')){
+	function array_intersect_key(){
+		$array_args = func_get_args();
+		$array_result = array_shift( $array_args );
+		foreach( (array)$array_args as $array_check ) {
+			foreach( (array)$array_result as $mixed_key => $mixed_value) {
+				if( !array_key_exists( $mixed_key, $array_check ) ) {
+					unset($array_result[$mixed_key]);
+				}
+			}
+		}
+		return $array_result;
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// PHP STRING : STR_PAD_?
+if( ! function_exists('str_pad_right') ){
+	function str_pad_right( $propertyInput, $propertyLength, $propertyPad = " " ){
+		return str_pad( $propertyInput, $propertyLength, $propertyPad, STR_PAD_RIGHT );
+	}
+}
+if( ! function_exists('str_pad_left') ){
+	function str_pad_left( $propertyInput, $propertyLength, $propertyPad = " " ){
+		return str_pad( $propertyInput, $propertyLength, $propertyPad, STR_PAD_LEFT );
+	}
+}
+if( ! function_exists('str_pad_both') ){
+	function str_pad_both( $propertyInput, $propertyLength, $propertyPad = " " ){
+		return str_pad( $propertyInput, $propertyLength, $propertyPad, STR_PAD_BOTH );
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// PHP STRING : CHR_?
+if( ! function_exists('chr_unicode') ){
+	function chr_unicode( $propertyAsciiValue ){
+		if ($propertyAsciiValue <= 0x7F)
+			{ return chr($propertyAsciiValue); }
+		else if ($propertyAsciiValue <= 0x7FF)
+			{ return chr(0xC0 | $propertyAsciiValue >> 6) .chr(0x80 | $propertyAsciiValue & 0x3F); }
+		else if ($propertyAsciiValue <= 0xFFFF)
+			{ return chr(0xE0 | $propertyAsciiValue >> 12).chr(0x80 | $propertyAsciiValue >> 6 & 0x3F) .chr(0x80 | $propertyAsciiValue & 0x3F); }
+		else if ($propertyAsciiValue <= 0x10FFFF)
+			{ return chr(0xF0 | $propertyAsciiValue >> 18).chr(0x80 | $propertyAsciiValue >> 12 & 0x3F).chr(0x80 | $propertyAsciiValue >> 6 & 0x3F).chr(0x80 | $propertyAsciiValue & 0x3F); }
+		else { return false; }
+	}
+}
+?>
