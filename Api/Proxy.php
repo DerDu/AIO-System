2295c981f0df46b3d1c807959da67eedf0a4a6c4
DerDu <blood-demon@gmx.net>
2011-02-24 08:58:58 +0100

    [Add] Proxy support

---
+++ Api/Proxy.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * This file contains the API:Proxy
+ *
+ // ---------------------------------------------------------------------------------------
+ * LICENSE (BSD)
+ *
+ * Copyright (c) 2011, Gerd Christian Kunze
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the Gerd Christian Kunze nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ---------------------------------------------------------------------------------------
+ *
+ * @package AioSystem\Api
+ */
+namespace AioSystem\Api;
+use AioSystem\Core\ClassProxyFile as AioProxy;
+/**
+ * ClassProxy
+ *  
+ * @package AioSystem\Api
+ */
+class ClassProxy {
+	/**
+	 * @static
+	 * @param string $Host
+	 * @param string $Port
+	 * @param null|string $User
+	 * @param null|string $Password
+	 * @return void
+	 */
+	public static function Proxy( $Host, $Port, $User = null, $Password = null ) {
+		AioProxy::setFileProxy( $Host, $Port, $User, $Password );
+	}
+	/**
+	 * @static
+	 * @param string $Url
+	 * @return null|string
+	 */
+	public static function HttpGet( $Url ) {
+		return AioProxy::getFileProxy( $Url );
+	}
+}
+?>
