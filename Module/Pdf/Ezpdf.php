a1a8f8ac9464bcdf7e263c466aaf661eb46e9845
DerDu <blood-demon@gmx.net>
2011-03-03 08:46:11 +0100

    [Code Cleanup]

--- Module/Pdf/Ezpdf.php
+++ Module/Pdf/Ezpdf.php
@@ -74,8 +74,7 @@ interface InterfaceEzpdf {
  * @package AioSystem\Module
  * @subpackage Pdf
  */
-class ClassEzpdf implements InterfaceEzpdf
-{
+class ClassEzpdf implements InterfaceEzpdf {
 	/** @var $ezpdf_instance \Cezpdf */
 	private static $ezpdf_instance = null;
 	private static $ezpdf_filename = null;
@@ -104,12 +103,10 @@ class ClassEzpdf implements InterfaceEzpdf
 // ---------------------------------------------------------------------------------------
 	public function __construct() {
 	}
-	public static function ezpdf_utf8( $string_utf8_encode )
-	{
+	public static function ezpdf_utf8( $string_utf8_encode ) {
 		return utf8_decode( str_replace( '€', chr(128), $string_utf8_encode ) );
 	}
-	public static function ezpdf_table_map( $array_content, $array_columns, $string_callback )
-	{
+	public static function ezpdf_table_map( $array_content, $array_columns, $string_callback ) {
 		foreach( (array)$array_content as $index_row => $array_row ){
 			foreach( (array)$array_row as $string_column => $mixed_column ){
 				if( in_array( $string_column, (array)$array_columns ) ){
@@ -120,8 +117,7 @@ class ClassEzpdf implements InterfaceEzpdf
 		return $array_content;
 	}
 // ezPDF : Open/Close --------------------------------------------------------------------
-	public static function ezpdf_open( $string_filename )
-	{
+	public static function ezpdf_open( $string_filename ) {
 		if( !class_exists( 'Cezpdf' ) ) require_once(__DIR__ . '/Ezpdf/class.ezpdf.php');
 		self::ezpdf_instance( new \Cezpdf() );
 		self::ezpdf_filename( $string_filename );
@@ -129,15 +125,13 @@ class ClassEzpdf implements InterfaceEzpdf
 		self::ezpdf_font_color();
 		self::ezpdf_line_color();
 	}
-	public static function ezpdf_close()
-	{
+	public static function ezpdf_close() {
 		$ClassSystemFile = \AioSystem\Core\ClassSystemFile::Instance( self::ezpdf_filename() );
 		$ClassSystemFile->propertyFileContent( self::ezpdf_instance()->ezOutput() );
 		$ClassSystemFile->writeFile();
 	}
 // ezPDF : FONT --------------------------------------------------------------------------
-	public static function ezpdf_font_family( $string_filename )
-	{
+	public static function ezpdf_font_family( $string_filename ) {
 		if( strtoupper( pathinfo( $string_filename, PATHINFO_EXTENSION ) ) == 'TTF' ) {
 			$string_filename = \AioSystem\Library\ClassFont::convertTtf2Afm( $string_filename );
 		}
@@ -151,32 +145,27 @@ class ClassEzpdf implements InterfaceEzpdf
 			);
 		self::ezpdf_instance()->selectFont( $string_filename );
 	}
-	public static function ezpdf_font_size( $integer_size = null )
-	{
+	public static function ezpdf_font_size( $integer_size = null ) {
 		// default: 12
 		if( $integer_size !== null ) self::$ezpdf_option_font_size = $integer_size;
 		return self::$ezpdf_option_font_size;
 	}
-	public static function ezpdf_font_align( $string_align = null )
-	{
+	public static function ezpdf_font_align( $string_align = null ) {
 		// default: left (right, center, centre, full)
 		if( $string_align !== null ) self::$ezpdf_option_font_align = $string_align;
 		return self::$ezpdf_option_font_align;
 	}
-	public static function ezpdf_font_color( $string_hexcolor = null )
-	{
+	public static function ezpdf_font_color( $string_hexcolor = null ) {
 		// default: 000000
 		if( $string_hexcolor !== null ) self::$ezpdf_option_font_color = $string_hexcolor;
 		return self::$ezpdf_option_font_color;
 	}
-	public static function ezpdf_font_lineheight( $float_lineheight = null )
-	{
+	public static function ezpdf_font_lineheight( $float_lineheight = null ) {
 		// default: 1
 		if( $float_lineheight !== null ) self::$ezpdf_option_font_lineheight = $float_lineheight;
 		return self::$ezpdf_option_font_lineheight;
 	}
-	public static function ezpdf_font_margin( $float_margintop = null, $float_marginright = null, $float_marginbottom = null, $float_marginleft = null )
-	{
+	public static function ezpdf_font_margin( $float_margintop = null, $float_marginright = null, $float_marginbottom = null, $float_marginleft = null ) {
 		// default: 0
 		if( $float_margintop !== null ) self::$ezpdf_option_font_margintop = $float_margintop;
 		if( $float_marginright !== null ) self::$ezpdf_option_font_marginright = $float_marginright;
@@ -185,23 +174,19 @@ class ClassEzpdf implements InterfaceEzpdf
 		return array( self::$ezpdf_option_font_margintop, self::$ezpdf_option_font_marginright, self::$ezpdf_option_font_marginbottom, self::$ezpdf_option_font_marginleft );
 	}
 // ezPDF : PAGE --------------------------------------------------------------------------
-	public static function ezpdf_page_margin( $float_cm_top, $float_cm_right, $float_cm_bottom, $float_cm_left )
-	{
+	public static function ezpdf_page_margin( $float_cm_top, $float_cm_right, $float_cm_bottom, $float_cm_left ) {
 		self::ezpdf_instance()->ezSetCmMargins( $float_cm_top, $float_cm_bottom, $float_cm_left, $float_cm_right );
 	}
-	public static function ezpdf_newpage()
-	{
+	public static function ezpdf_newpage() {
 		self::ezpdf_instance()->ezNewPage();
 	}
-	public static function ezpdf_page_movecursor( $integer_units, $bool_force = false )
-	{
+	public static function ezpdf_page_movecursor( $integer_units, $bool_force = false ) {
 		// ( * - 1 ) -> Bottom Up Y ( e.g. 10 Units Down = -10 )
 		self::ezpdf_instance()->ezSetDy( ($integer_units * -1), ($bool_force?'makeSpace':'') );
 	}
 
 // ezPDF : TABLE -------------------------------------------------------------------------
-	public static function ezpdf_table( $array_content, $array_columns = '', $string_title = '', $debug_extension = array() )
-	{
+	public static function ezpdf_table( $array_content, $array_columns = '', $string_title = '', $debug_extension = array() ) {
 		$array_align = self::ezpdf_table_align();
 		$array_width = self::ezpdf_table_width();
 		return self::ezpdf_instance()->ezTable( $array_content,
@@ -223,8 +208,7 @@ class ClassEzpdf implements InterfaceEzpdf
 			), $debug_extension )
 		);
 	}
-	public static function ezpdf_table_align( $string_align = null )
-	{
+	public static function ezpdf_table_align( $string_align = null ) {
 		// default: center (left, right, centre, full)
 		if( $string_align !== null ) self::$ezpdf_option_table_align = $string_align;
 		$array_padding = self::ezpdf_table_padding();
@@ -244,8 +228,7 @@ class ClassEzpdf implements InterfaceEzpdf
 			default: return array('center','center');
 		}
 	}
-	public static function ezpdf_table_width( $float_width_percent = null, $float_max_percent = null )
-	{
+	public static function ezpdf_table_width( $float_width_percent = null, $float_max_percent = null ) {
 		if( $float_width_percent !== null ) self::$ezpdf_option_table_width = $float_width_percent;
 		if( $float_max_percent !== null ) self::$ezpdf_option_table_width_max = $float_max_percent;
 		// default 'width'=>0, 'maxWidth'=>0
@@ -254,32 +237,27 @@ class ClassEzpdf implements InterfaceEzpdf
 			(self::$ezpdf_option_table_width_max>0)?((self::ezpdf_attribute_getPageWidth()-self::ezpdf_attribute_getMarginLeft()-self::ezpdf_attribute_getMarginRight())/100*self::$ezpdf_option_table_width_max):0
 		);
 	}
-	public static function ezpdf_table_padding( $float_padding_row = null, $float_padding_column = null )
-	{
+	public static function ezpdf_table_padding( $float_padding_row = null, $float_padding_column = null ) {
 		// default: 2
 		if( $float_padding_row !== null ) self::$ezpdf_option_table_padding_row = $float_padding_row;
 		// default: 5
 		if( $float_padding_column !== null ) self::$ezpdf_option_table_padding_column = $float_padding_column;
 		return array( self::$ezpdf_option_table_padding_row, self::$ezpdf_option_table_padding_column );
 	}
-	public static function ezpdf_table_showlines( $integer_showlines = null )
-	{
+	public static function ezpdf_table_showlines( $integer_showlines = null ) {
 		if( $integer_showlines !== null ) self::$ezpdf_option_table_showlines = $integer_showlines;
 		return self::$ezpdf_option_table_showlines;
 	}
-	public static function ezpdf_table_showheader( $integer_showheader = null )
-	{
+	public static function ezpdf_table_showheader( $integer_showheader = null ) {
 		if( $integer_showheader !== null ) self::$ezpdf_option_table_showheader = $integer_showheader;
 		return self::$ezpdf_option_table_showheader;
 	}
-	public static function ezpdf_table_showshade( $integer_showshade = null )
-	{
+	public static function ezpdf_table_showshade( $integer_showshade = null ) {
 		if( $integer_showshade !== null ) self::$ezpdf_option_table_showshade = $integer_showshade;
 		return self::$ezpdf_option_table_showshade;
 	}
 // ezPDF : TEXT --------------------------------------------------------------------------
-	public static function ezpdf_text( $string_text )
-	{
+	public static function ezpdf_text( $string_text ) {
 		$array_rgbcolor = \AioSystem\Library\ClassColor::convertHex2RgbFloat( self::$ezpdf_option_font_color );
 		self::ezpdf_instance()->setColor($array_rgbcolor[0],$array_rgbcolor[1],$array_rgbcolor[2]);
 
@@ -298,13 +276,11 @@ class ClassEzpdf implements InterfaceEzpdf
 		self::ezpdf_page_movecursor( self::ezpdf_attribute_getFontMarginBottom() );
 	}
 // ezPDF : IMAGE -------------------------------------------------------------------------
-	public static function ezpdf_image( $string_filename )
-	{
+	public static function ezpdf_image( $string_filename ) {
 		return self::ezpdf_instance()->ezImage( $string_filename );
 	}
 // ---------------------------------------------------------------------------------------
-	public static function ezpdf_line()
-	{
+	public static function ezpdf_line() {
 		self::ezpdf_page_movecursor( self::ezpdf_attribute_getLineMarginTop() );
 
 		self::ezpdf_line_color();
@@ -324,29 +300,25 @@ class ClassEzpdf implements InterfaceEzpdf
 
 		self::ezpdf_page_movecursor( self::ezpdf_attribute_getLineMarginBottom() );
 	}
-	public static function ezpdf_line_color( $string_hexcolor = null )
-	{
+	public static function ezpdf_line_color( $string_hexcolor = null ) {
 		// default: 000000
 		if( $string_hexcolor !== null ) self::$ezpdf_option_line_color = $string_hexcolor;
 		$array_rgbcolor = \AioSystem\Library\ClassColor::convertHex2RgbFloat( self::$ezpdf_option_line_color );
 		self::ezpdf_instance()->setStrokeColor($array_rgbcolor[0],$array_rgbcolor[1],$array_rgbcolor[2]);
 		return self::$ezpdf_option_line_color;
 	}
-	public static function ezpdf_line_margin( $float_margintop = null, $float_marginbottom = null )
-	{
+	public static function ezpdf_line_margin( $float_margintop = null, $float_marginbottom = null ) {
 		// default: 0
 		if( $float_margintop !== null ) self::$ezpdf_option_line_margintop = $float_margintop;
 		if( $float_marginbottom !== null ) self::$ezpdf_option_line_marginbottom = $float_marginbottom;
 		return array( self::$ezpdf_option_line_margintop, self::$ezpdf_option_line_marginbottom );
 	}
 // ---------------------------------------------------------------------------------------
-	private static function ezpdf_filename( $string_filename = null )
-	{
+	private static function ezpdf_filename( $string_filename = null ) {
 		if( $string_filename !== null ) self::$ezpdf_filename = $string_filename;
 		return self::$ezpdf_filename;
 	}
-	public static function ezpdf_instance( $object_ezpdf = null )
-	{
+	public static function ezpdf_instance( $object_ezpdf = null ) {
 		if( $object_ezpdf !== null ) self::$ezpdf_instance = $object_ezpdf;
 		return self::$ezpdf_instance;
 	}
