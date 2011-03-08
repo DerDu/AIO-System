624458dcdd454e47778d7a558753a76d889c8d71
DerDu <blood-demon@gmx.net>
2011-03-08 15:08:49 +0100

    [Add] Video (play) support

---
+++ Module/Video/Video.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Video
+ *
+ // ---------------------------------------------------------------------------------------
+ * LICENSE (BSD)
+ *
+ * Copyright (c) 2011, Gerd Christian Kunze
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the Gerd Christian Kunze nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ---------------------------------------------------------------------------------------
+ *
+ * @package AioSystem\Module
+ * @subpackage Video
+ */
+namespace AioSystem\Module\Video;
+use AioSystem\Core\ClassSystemDirectory as Directory;
+/**
+ * @package AioSystem\Module
+ * @subpackage Video
+ */
+interface InterfaceVideo {
+	public static function Load( $string_filename, $array_option = array() );
+}
+/**
+ * @package AioSystem\Module
+ * @subpackage Video
+ */
+class ClassVideo implements InterfaceVideo
+{
+	public static function Load( $string_filename, $array_option = array() )
+	{
+		$array_allowed_option = array(
+			'width'=>320,'height'=>180,
+			'autoplay'=>true,'loop'=>true,'controls'=>true,
+			'slide_speed'=>6.0, 'fade_speed'=>3.0,
+			'background'=>'#000000',
+			'ajaxload'=>false
+		);
+		$array_option = array_merge( $array_allowed_option, $array_option );
+		$array_option['player-id'] = sha1(microtime(true));
+
+		if( !is_file( $string_filename ) && is_dir( $string_filename ) )
+		return self::_imageSlide( $string_filename, $array_option );
+
+		if( !class_exists( __NAMESPACE__.'\ClassVideoDriver') ) require_once(dirname(__FILE__) . '/VideoDriver.php');
+		$string_extension = strtolower( pathinfo( trim( $string_filename ), PATHINFO_EXTENSION ) );
+		if( method_exists( __NAMESPACE__.'\ClassVideoDriver', 'driver_'.$string_extension ) ){
+			if( $array_option['ajaxload'] ) {
+				$string_player_markup = '<div id="module-video-player-'.$array_option['player-id'].'" style="background:'.$array_option['background'].';width:'.$array_option['width'].'px;height:'.$array_option['height'].'px;"></div>';
+				$string_player_markup .= '<script type="text/javascript">';
+					$string_player_markup .= 'jQuery(document).ready(function() {';
+						$string_player_markup .= 'jQuery(\'div#module-video-player-'.$array_option['player-id'].'\').html(\''.call_user_func_array( 'ClassVideoDriver::driver_'.$string_extension, array( $string_filename, $array_option ) ).'\');';
+					$string_player_markup .= '});';
+				$string_player_markup .= '</script>';
+				return $string_player_markup;
+			} else {
+				return call_user_func_array( __NAMESPACE__.'\ClassVideoDriver::driver_'.$string_extension, array( $string_filename, $array_option ) );
+			}
+		} else {
+			throw new \Exception( 'Driver not available!' );
+		}
+	}
+	private static function _imageSlide( $string_directory, $array_option )
+	{
+		$array_files = Directory::getFileList( $string_directory, array('jpg','png','jpeg','bmp','tif') );
+
+		$str_return = '<div id="module-video-player-'.$array_option['player-id'].'" style="width: '.$array_option['width'].'px; height: '.$array_option['height'].'px; position: relative;">
+		<img alt="" src="'.$array_files[0]->propertyFileLocation().'" style=" position: absolute; width: '.$array_option['width'].'px; height: '.$array_option['height'].'px; display: none;" />
+		<img alt="" src="'.$array_files[0]->propertyFileLocation().'" style=" position: absolute; width: '.$array_option['width'].'px; height: '.$array_option['height'].'px; display: none;" />
+		</div>';
+		$array_slide['definition'] = '';
+		$array_slide['preload'] = '';
+		/** @var $object_file \AioSystem\Core\ClassSystemFile */
+		foreach ( (array) $array_files as $integer_file => $object_file ){
+			$array_slide['definition'] .= ' arr_slide['.$integer_file.'] = "'.$object_file->propertyFileLocation().'"; ';
+			$array_slide['preload'] .= ' preload_slide.src = "'.$object_file->propertyFileLocation().'"; ';
+		}
+		$array_option['fade_speed'] = ($array_option['fade_speed']*1000);
+		$array_option['slide_speed'] = ($array_option['slide_speed']*1000);
+
+		$str_return .= '<script type="text/javascript">
+			jQuery(\'div#module-video-player-'.$array_option['player-id'].'\').hide();
+			jQuery(\'div#module-video-player-'.$array_option['player-id'].'\').css({background:\''.$array_option['background'].'\'});
+			jQuery(\'div#module-video-player-'.$array_option['player-id'].'\').fadeIn('.$array_option['fade_speed'].');
+			function module_video_player_slide_'.$array_option['player-id'].'( int_slide, flg_img )
+			{
+				var object_player = jQuery(\'div#module-video-player-'.$array_option['player-id'].'\');
+				var preload_slide = new Image(); '.$array_slide['preload'].'
+				var arr_slide = new Array(); '.$array_slide['definition'].'
+
+				if( flg_img == 0 ) { flg_img = 1; } else { flg_img = 0; }
+				if( flg_img ) {
+					object_player.find(\'img:eq(1)\').attr(\'src\',arr_slide[int_slide]).fadeIn('.$array_option['fade_speed'].');
+					object_player.find(\'img:eq(0)\').fadeOut('.$array_option['fade_speed'].');
+				} else {
+					object_player.find(\'img:eq(0)\').attr(\'src\',arr_slide[int_slide]).fadeIn('.$array_option['fade_speed'].');
+					object_player.find(\'img:eq(1)\').fadeOut('.$array_option['fade_speed'].');
+				}
+				if( arr_slide.length == 1 ) return;
+
+				int_slide += 1; if( int_slide >= arr_slide.length ) int_slide = 0;
+				window.setTimeout("module_video_player_slide_'.$array_option['player-id'].'( "+int_slide+", "+flg_img+" )",'.$array_option['slide_speed'].');
+			}';
+		if( $array_option['autoplay'] ) {
+			is_bool( $array_option['autoplay'] ) ? $int_autoplay = 1000 : $int_autoplay = $array_option['autoplay'];
+			$str_return .= 'window.setTimeout("module_video_player_slide_'.$array_option['player-id'].'(0,0);",'.$int_autoplay.');';
+		}
+		return $str_return.'</script>';
+	}
+	/*
+	  *
+	 * 	function module_video_img2swf( $val_parameter = "" )
+	{
+		// ----------------------
+		// Fetch Parameter
+			$val_parameter = system::system_parameter( $val_parameter, "file", array("file"), array(), __CLASS__.".".__FUNCTION__ );
+		// ----------------------
+
+			$arr_file = system::file_info( $val_parameter[file] );
+
+			switch ( $arr_file[extension] )
+			{
+				case "png":
+					{
+						if( !file_exists( $arr_file[path]."/".$arr_file[name].".dbl" ) )
+						$val_parameter[file] = module_image::module_image_convert( array("file"=>$val_parameter[file],"type"=>"dbl") );
+						else
+						$val_parameter[file] = $arr_file[path]."/".$arr_file[name].".dbl";
+						break;
+					}
+				default :
+					{
+						break;
+					}
+			}
+			$val_image = file_get_contents( $val_parameter[file] );
+
+			$obj_shape = new swfshape();
+			$obj_image = new swfbitmap( $val_image );
+			$res_image = $obj_shape->addfill( $obj_image );
+
+			$obj_shape->setrightfill( $res_image );
+			$obj_shape->drawLine( $obj_image->getwidth(),0 );
+			$obj_shape->drawLine( 0, $obj_image->getheight() );
+			$obj_shape->drawLine( ($obj_image->getwidth()*-1), 0 );
+			$obj_shape->drawLine( 0, ($obj_image->getheight()*-1) );
+
+			$int_width = $obj_image->getWidth();
+			$int_height = $obj_image->getHeight();
+
+			$res_movie = new swfmovie();
+			$res_movie->setDimension( $int_width, $int_height );
+			$res_movie->add( $obj_shape );
+
+			$res_movie->save( $arr_file[path]."/".$arr_file[name].".swf" );
+
+			return $arr_file[path]."/".$arr_file[name].".swf";
+	}
+	 */
+}
+?>
---
+++ Module/Video/VideoDriver.php
@@ -0,0 +1,141 @@
+<?php
+/**
+ * VideoDriver
+ *
+ // ---------------------------------------------------------------------------------------
+ * LICENSE (BSD)
+ *
+ * Copyright (c) 2011, Gerd Christian Kunze
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the name of the Gerd Christian Kunze nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ---------------------------------------------------------------------------------------
+ *
+ * @package AioSystem\Module
+ * @subpackage Video
+ */
+namespace AioSystem\Module\Video;
+use AioSystem\Core\ClassSystemDirectory as Directory;
+/**
+ * @package AioSystem\Module
+ * @subpackage Video
+ */
+class ClassVideoDriver {
+// Windows Media Player ------------------------------------------------------------------
+	public static function driver_wmv( $string_filename, $array_option = array() )
+	{
+		return '<object type="video/x-ms-wmv" data="'.$string_filename.'" width="'.$array_option['width'].'" height="'.$array_option['height'].'">
+				<param name="src" value="'.$string_filename.'" />
+				<param name="autostart" value="'.$array_option['autoplay'].'" />
+				<param name="controller" value="'.$array_option['controls'].'" />
+				</object>';
+	}
+	public static function driver_asx( $string_filename, $array_option = array() )
+	{
+		return self::driver_wmv( $string_filename, $array_option );
+	}
+	public static function driver_asf( $string_filename, $array_option = array() )
+	{
+		return self::driver_wmv( $string_filename, $array_option );
+	}
+	public static function driver_avi( $string_filename, $array_option = array() )
+	{
+		return self::driver_wmv( $string_filename, $array_option );
+	}
+// Real Player ---------------------------------------------------------------------------
+	public static function driver_rmv( $string_filename, $array_option = array() )
+	{
+		return '<object id="myMovie" classid="clsid:CFCDAA03-8BE4-11cf-B84B-0020AFBBCCFA" width="'.$array_option['width'].'" height="'.$array_option['height'].'">
+					<param name="src" value="'.$string_filename.'" />
+					<param name="console" value="video1" />
+					<param name="controls" value="'.($array_option['controls']?'all':'false').'" />
+					<param name="autostart" value="'.$array_option['autoplay'].'" />
+					<param name="loop" value="'.$array_option['loop'].'" />
+					<embed name="myMovie" src="'.$string_filename.'" width="'.$array_option['width'].'" height="'.$array_option['height'].'"
+					autostart="'.$array_option['autoplay'].'" loop="false" nojava="false" console="video1" controls="'.($array_option['controls']?'controlpanel':'false').'">
+					</embed>
+					<noembed><a href="'.$string_filename.'">Play first clip</a></noembed>
+				</object>';
+	}
+	public static function driver_rmvb( $string_filename, $array_option = array() )
+	{
+		return self::driver_rmv( $string_filename, $array_option );
+	}
+// Quicktime -----------------------------------------------------------------------------
+	public static function driver_mov( $string_filename, $array_option = array() )
+	{
+		return '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="'.$array_option['width'].'" height="'.$array_option['height'].'">
+					<param name="src" value="'.$string_filename.'" />
+					<param name="controller" value="'.$array_option['controls'].'" />
+					<param name="autoplay" value="'.$array_option['autoplay'].'" />
+					<!--[if !IE]>-->
+						<object type="video/quicktime"
+						data="'.$string_filename.'"
+						width="'.$array_option['width'].'" height="'.$array_option['height'].'">
+						<param name="autoplay" value="'.$array_option['autoplay'].'" />
+						<param name="controller" value="'.$array_option['controls'].'" />
+						</object>
+					<!--<![endif]-->
+				</object>';
+	}
+// MPEG ----------------------------------------------------------------------------------
+	public static function driver_mpg( $string_filename, $array_option = array() )
+	{
+		return '<embed src="'.$string_filename.'" autostart='.$array_option['autoplay'].' loop='.($array_option['loop']?'true':'false').' controller='.$array_option['controls'].' width="'.$array_option['width'].'" height="'.$array_option['height'].'" />';
+	}
+	public static function driver_mpeg( $string_filename, $array_option = array() )
+	{
+		return self::driver_mpg( $string_filename, $array_option );
+	}
+// Flash Video Player --------------------------------------------------------------------
+	public static function driver_flv( $string_filename, $array_option = array() )
+	{
+		return '<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>'
+				.'<div id="MyMovie"><a href="http://www.macromedia.com/go/getflashplayer">Baixe o flash player</a> para visualizar este.</div>
+				<script type="text/javascript">
+					var s1 = new SWFObject("http://ianitsky.com/files/flvplayer.swf", "single","'.$array_option['width'].'","'.$array_option['height'].'","7");
+					s1.addParam("allowfullscreen","true");
+					s1.addVariable("file","'.$string_filename.'");
+					s1.addVariable("showdigits", "'.$array_option['controls'].'");
+					s1.addVariable("autostart", "'.$array_option['autoplay'].'");
+					s1.write("MyMovie");
+				</script>';
+	}
+// Flash Player --------------------------------------------------------------------------
+	public static function driver_swf( $string_filename, $array_option = array() )
+	{
+		return '<object type="application/x-shockwave-flash" data="'.$string_filename.'" id="swf'.$array_option['player-id'].'" width="'.$array_option['width'].'" height="'.$array_option['height'].'">
+					<param name="movie" value="'.$string_filename.'" />
+					<param name="quality" value ="high" />
+					<param name="loop" value="'.($array_option['loop']?'true':'false').'" />
+					<param name="menu" value="'.$array_option['controls'].'" />
+					<param name="swliveconnect" value="true" />
+				</object>';
+	}
+}
+?>
