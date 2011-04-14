aa1242a8e11e05b204f27cff92d929307d2ff78d
DerDu <blood-demon@gmx.net>
2011-04-14 15:05:17 +0200

    [Code Cleanup]

--- Core/EventJournal.php
+++ Core/EventJournal.php
@@ -19,7 +19,7 @@
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
  *
- *  * Neither the name of the Gerd Christian Kunze nor the names of its
+ *  * Neither the name of Gerd Christian Kunze nor the names of the
  *    contributors may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
  *
@@ -39,7 +39,8 @@
  * @package AioSystem\Core
  * @subpackage Event
  */
-namespace AioSystem\Core;#
+namespace AioSystem\Core;
+use \AioSystem\Api\System as System;
 /**
  * @package AioSystem\Core
  * @subpackage Event
@@ -63,10 +64,10 @@ class ClassEventJournal implements InterfaceEventJournal {
 		if( !is_dir( dirname( $propertyJournalName ) ) ) {
 			ClassSystemDirectory::createDirectory( dirname( $propertyJournalName ) );
 		}
-		$CoreSystemFile = new ClassSystemFile( $propertyJournalName );
+		$CoreSystemFile = System::File( $propertyJournalName );
 		if( date( 'Ymd', $CoreSystemFile->propertyFileTime() ) < date('Ymd') ) {
 			$CoreSystemFile->moveFile( substr($propertyJournalName,0,-4).'.'.date('YmdHis').'.txt' );
-			$CoreSystemFile = new ClassSystemFile( $propertyJournalName );
+			$CoreSystemFile = System::File( $propertyJournalName );
 		}
 		$CoreSystemFile->propertyFileContent(
 					( $CoreSystemFile->propertyFileSize() != 0 ? "\n" : '' )
@@ -80,7 +81,7 @@ class ClassEventJournal implements InterfaceEventJournal {
 	/**
 	 * @static
 	 * @param int $propertyDayCountHistory
-	 * @return array
+	 * @return \AioSystem\Core\ClassSystemFile[] array
 	 */
 	public static function getJournalList( $propertyDayCountHistory = 15 ) {
 		$regexpIntegerBetween = \AioSystem\Library\ClassRegExp::integerBetween(
@@ -96,6 +97,84 @@ class ClassEventJournal implements InterfaceEventJournal {
 				'propertyFileTime'=>$regexpIntegerBetween
 			)
 		);
+	}
+
+	public static function getJournalContentList( $DayCountHistory = 15, $ShowCountHistory = 10, $ClassSystemFileList = null ) {
+		if( $ClassSystemFileList === null ) {
+			$ClassSystemFileList = self::getJournalList( $DayCountHistory );
+		}
+
+		usort( $ClassSystemFileList, create_function('$FileA,$FileB','return $FileA->propertyFileTime() < $FileB->propertyFileTime();') );
+		/** @var ClassSystemFile $ClassSystemFile */
+		$Content = '';
+		foreach( (array)$ClassSystemFileList as $ClassSystemFile ) {
+			$ClassSystemFile->propertyFileContent();
+			$Content .= "\n\n".trim(
+				preg_replace( '!\n\n(-{50})!is', "\n".$ClassSystemFile->propertyFileName().'${1}', $ClassSystemFile->propertyFileContent() )
+			)."\n".$ClassSystemFile->propertyFileName();
+		}
+		$Match = array();
+		preg_match_all(
+			'!([0-9]{2}\.[0-9]{2}\.[0-9]{4}) ([0-9]{2}:[0-9]{2}:[0-9]{2}) SID:(.*?)\n-{50}(.*?)(?=(-{50}|$))!is',
+			$Content,
+			$Match
+		);
+		$ErrorType = array(
+			'Code [0 DEBUG]' => array('#D7A700','DEBUG','#FFF2E0','#973700'),
+			'Code [2048 STRICT]' => array('#C0C0C0','STRICT','#FEFEFE','gray'),
+			'Code [8 NOTICE]' => array('#C0C0C0','NOTICE','#FEFEFE'),
+			'Code [2 WARNING]' => array('#903030','WARNING','#FFF3F3'),
+			'Unexpected Error:' => array('#D30D0D','UNEXPECTED','#FFD0D0'),
+			'ShutDown Error:' => array('#FF2323','SHUTDOWN','#FFADAD'),
+		);
+		$Length = count( $Match[0] );
+		$TemplateData = array();
+		for( $Run = 0; $Run < $Length && $Run < $ShowCountHistory; $Run++ ){
+			$Attribute = array();
+			foreach( (array)$ErrorType as $Type => $AttributeList ){
+				if( strpos($Match[4][$Run], $Type ) !== false ){
+					$Attribute = $AttributeList;
+					break;
+				}
+			}
+			array_push( $TemplateData, array(
+				'CategoryStyle' =>
+					'text-align:center;'
+					.(isset($Attribute[0])?'color:'.$Attribute[0].';':'')
+				,
+				'EntrySort' => date('YmdHis',strtotime( $Match[1][$Run].' '.$Match[2][$Run] )),
+				'EntryTimestamp' => trim($Match[1][$Run].' '.$Match[2][$Run]),
+				'CategoryType' => trim(isset($Attribute[1])?'<br/>'.$Attribute[1]:''),
+				'SessionId' => trim($Match[3][$Run]),
+				'SessionUser' => '',
+				'ContentStyle' =>
+					(isset($Attribute[3])?'color:'.$Attribute[3].';':'')
+					.(isset($Attribute[2])?'background-color:'.$Attribute[2].';':'')
+				,
+				'Content' =>trim(
+							preg_replace(
+								'!<br ?/?>\n([^<]*?)$!is',
+								'<span style="color:silver;font-size:0.8em;"><br/>(${1})</span>',
+							preg_replace(
+								'!<br ?/?>[^<]*?(?=<br ?/?>[^<]*?$)!is',
+								'<span style="color:gray;font-size:0.9em;">${0}</span>',
+							nl2br(utf8_encode(trim($Match[4][$Run])))
+							)
+				)),
+				'FileCount' => count( $ClassSystemFileList )
+			) );
+		}
+		return $TemplateData;
+	}
+
+	public static function getJournalContent( $DayCountHistory = 15, $ShowCountHistory = 10, $ClassSystemFileList = null ) {
+		$Template = \AioSystem\Api\Template::Load( __DIR__.'/EventJournal/JournalEntryTable.tpl' );
+		$TemplateData = self::getJournalContentList( $DayCountHistory, $ShowCountHistory, $ClassSystemFileList );
+
+		$Template->Repeat( 'JournalEntryList', $TemplateData );
+		$Template->Assign( 'FileCount', $TemplateData[0]['FileCount'] );
+		$Template->Assign( 'EntryCount', count( $TemplateData ) );
+		return $Template->Parse();
 	}
 }
 ?>
