da4fe363f880ee741ff9425e078fe20975c3d0ca
DerDu <blood-demon@gmx.net>
2011-04-26 16:38:50 +0200

    [Code Cleanup]

--- Core/SeoUri.php
+++ Core/SeoUri.php
@@ -45,10 +45,10 @@ namespace AIOSystem\Core;
  * @subpackage Seo
  */
 interface InterfaceSeoUri {
-	public static function uri_path( $string_path_relative, $integer_path_level = 0 );
-	public static function uri_carrier( $string_path_carrier = null );
-	public static function uri_separator( $string_separator_char = null );
-	public static function uri_request();
+	public static function UriPath( $string_path_relative, $integer_path_level = 0 );
+	public static function UriCarrier( $string_path_carrier = null );
+	public static function UriSeparator( $string_separator_char = null );
+	public static function UriRequest();
 }
 /**
  * @package AIOSystem\Core
@@ -59,7 +59,7 @@ class ClassSeoUri implements InterfaceSeoUri
 	private static $string_separator_char = '~';
 	private static $string_path_carrier = 'URI-PATH';
 	
-	public static function uri_path( $string_path_relative, $integer_path_level = 0 )
+	public static function UriPath( $string_path_relative, $integer_path_level = 0 )
 	{
 		if( !isset($_SERVER['SERVER_PORT']) ) $_SERVER['SERVER_PORT'] = '';
 
@@ -80,44 +80,44 @@ class ClassSeoUri implements InterfaceSeoUri
 			.$string_path_relative 
 		);
 	}
-	public static function uri_request()
+	public static function UriRequest()
 	{
-		$array_uri_filter = self::uri_filter( self::uri_string() );
+		$array_uri_filter = self::UriFilter( self::UriString() );
 		foreach( (array)$array_uri_filter as $string_uri_content )
 		{
-			$array_uri_content = explode( self::uri_separator(), $string_uri_content );
+			$array_uri_content = explode( self::UriSeparator(), $string_uri_content );
 			if( preg_match( '!^[a-zA-Z_\x7f-\xff\-\:][a-zA-Z0-9_\x7f-\xff\-\:]*$!', urldecode( $array_uri_content[0] ) ) )
 			{
 				$_REQUEST[urldecode( $array_uri_content[0] )] = urldecode( $array_uri_content[1] );
 			}
 		}
 		// KILL PARAMETER, KILL QUESTION-STRING, KILL TRAILING-SLASH
-		$_REQUEST[self::uri_carrier()] = preg_replace(
-			array('![\w\d\s\.\-\:]*?'.self::uri_separator().'[\w\d\s\.\-\:]*?(/|(?=\?)|$)!is','!\?.*$!is','!\/$!is'),
-			'', urldecode( self::uri_string() )
+		$_REQUEST[self::UriCarrier()] = preg_replace(
+			array('![\w\d\s\.\-\:]*?'.self::UriSeparator().'[\w\d\s\.\-\:]*?(/|(?=\?)|$)!is','!\?.*$!is','!\/$!is'),
+			'', urldecode( self::UriString() )
 		);
 	}
-	public static function uri_carrier( $string_path_carrier = null ) {
+	public static function UriCarrier( $string_path_carrier = null ) {
 		if( $string_path_carrier !== null ) self::$string_path_carrier = $string_path_carrier; return self::$string_path_carrier;
 	}
-	public static function uri_separator( $string_separator_char = null ) {
+	public static function UriSeparator( $string_separator_char = null ) {
 		if( $string_separator_char !== null ) self::$string_separator_char = $string_separator_char; return self::$string_separator_char;
 	}
 
-	private static function uri_string()
+	private static function UriString()
 	{
 		return substr( $_SERVER['REQUEST_URI'], strlen( $_SERVER['SCRIPT_NAME'] ) );
 	}
-	private static function uri_filter( $filter_seo_uri )
+	private static function UriFilter( $filter_seo_uri )
 	{
 		// CUT QUESTION-STRING 
 		$filter_seo_uri = explode( '?', $filter_seo_uri );
 		$filter_seo_uri = explode( '/', $filter_seo_uri[0] );
 		$filter_seo = create_function( '$filter_seo_uri',
 		'return preg_match("!^[^'
-		.self::uri_separator().']+?'
-		.self::uri_separator().'[^'
-		.self::uri_separator().']*$!is", $filter_seo_uri );' );
+		.self::UriSeparator().']+?'
+		.self::UriSeparator().'[^'
+		.self::UriSeparator().']*$!is", $filter_seo_uri );' );
 		return array_filter( $filter_seo_uri, $filter_seo );
 	}
 }
