4f4a904537da06d103b5ed9c20c289d242085f46
DerDu <blood-demon@gmx.net>
2011-04-29 12:00:47 +0200

    [Fix] __wakeup did not work for multiple routes

--- Module/Database/Database.php
+++ Module/Database/Database.php
@@ -46,8 +46,7 @@ use \AIOSystem\Api\Xml;
  * @package AIOSystem\Module
  * @subpackage Database
  */
-interface InterfaceDatabase
-{
+interface InterfaceDatabase {
 	public static function database_open( $string_hosttype, $string_hostname, $string_username, $string_password, $string_database );
 	public static function database_route( $string_database_route = null );
 	public static function database_list();
@@ -85,16 +84,13 @@ class ClassDatabase implements InterfaceDatabase
 
 	public function __construct() {
 	}
-
-	public static function database_adodb5()
-	{
+	public static function database_adodb5() {
 		return self::database_stage()->adodb5_object();
 	}
-
-	private static function database_session_set(){
+	private static function database_session_set() {
 		Session::Write( 'AIO-Database[ROUTE]',
 			\AIOSystem\Library\ClassEncryption::encodeSessionEncryption(
-				serialize( self::database_route() )
+				serialize( self::database_list() )
 			)
 		);
 		Session::Write( 'AIO-Database[STAGE]',
@@ -103,16 +99,22 @@ class ClassDatabase implements InterfaceDatabase
 			)
 		);
 	}
-	private static function database_session_get(){
+	private static function database_session_get() {
 		if( self::$database_route === null ){
-			$string_route = unserialize(
+			$array_route = unserialize(
 			\AIOSystem\Library\ClassEncryption::decodeSessionEncryption(
 				Session::Read( 'AIO-Database[ROUTE]' )
 			));
-			if( substr( $string_route, 0, 5 ) == 'ROUTE' ){
-				self::$database_route = $string_route;
+			self::$database_route = array();
+			foreach( (array)$array_route as $string_route ) {
+				if( substr( $string_route, 0, 5 ) == 'ROUTE' ){
+					self::$database_route = $string_route;
+				}
 			}
+		} else {
+			$array_route = array( self::$database_route );
 		}
+
 		if( count( self::$database_stage ) < 1 ){
 			// Load Class
 			if( !class_exists('cls__shell_adodb5') ) {
@@ -122,9 +124,14 @@ class ClassDatabase implements InterfaceDatabase
 				}
 			}
 			// Load Driver
-			if( !class_exists( 'ADODB_'.strtolower(self::database_route_engine()) ) ) {
-				require_once( __DIR__.('/Adodb/drivers/adodb-'.strtolower(self::database_route_engine()).'.inc.php') );
+			foreach( (array)$array_route as $string_route ) {
+				self::$database_route = $string_route;
+				if( !class_exists( 'ADODB_'.strtolower(self::database_route_engine()) ) ) {
+					//var_dump('Load Driver '.__DIR__.('/Adodb/drivers/adodb-'.strtolower(self::database_route_engine()).'.inc.php'));
+					require_once( __DIR__.('/Adodb/drivers/adodb-'.strtolower(self::database_route_engine()).'.inc.php') );
+				}
 			}
+
 			$array_stage = unserialize(
 			\AIOSystem\Library\ClassEncryption::decodeSessionEncryption(
 				Session::Read( 'AIO-Database[STAGE]' )
@@ -134,9 +141,7 @@ class ClassDatabase implements InterfaceDatabase
 			}
 		}
 	}
-
-	public static function database_open( $string_hosttype, $string_hostname, $string_username, $string_password, $string_database )
-	{
+	public static function database_open( $string_hosttype, $string_hostname, $string_username, $string_password, $string_database ) {
 		// Engine: Module-XMLFFDB ?
 		//if( strtoupper( $string_hosttype ) == 'XMLFFDB' ){
 			//if( !class_exists( 'cls__module_xmlffdb_engine' ) ) require_once( __DIR__.'/../module/module.xmlffdb_engine.php' );
@@ -153,51 +158,42 @@ class ClassDatabase implements InterfaceDatabase
 
 		return self::$database_route;
 	}
-	public static function database_list()
-	{
+	public static function database_list() {
 		self::database_session_get();
 		return array_keys( self::$database_stage );
 	}
-	public static function database_route( $string_database_default = null )
-	{
+	public static function database_route( $string_database_default = null ) {
 		if( $string_database_default !== null ) self::$database_route = $string_database_default;
 		return self::$database_route;
 	}
-	public static function database_route_engine()
-	{
+	public static function database_route_engine() {
 		preg_match( '!(?<=\[ENGINE:).*?(?=:HOST:)!is', self::database_route(), $array_match );
 		return $array_match[0];
 	}
-	public static function database_route_host()
-	{
+	public static function database_route_host() {
 		preg_match( '!(?<=:HOST:).*?(?=:DB:)!is', self::database_route(), $array_match );
 		return $array_match[0];
 	}
-	public static function database_route_database()
-	{
+	public static function database_route_database() {
 		preg_match( '!(?<=:DB:).*?(?=:USER:)!is', self::database_route(), $array_match );
 		return $array_match[0];
 	}
-	public static function database_route_user()
-	{
+	public static function database_route_user() {
 		preg_match( '!(?<=:USER:).*?(?=\])!is', self::database_route(), $array_match );
 		return $array_match[0];
 	}
-	public static function database_execute( $string_statement, $bool_cached = false )
-	{
+	public static function database_execute( $string_statement, $bool_cached = false ) {
 		self::database_mtimeout(true);
 		$array_result = self::database_stage()->executeAdodb( $string_statement, $bool_cached );
 		self::database_mtimeout(false);
 		return $array_result;
 	}
 
-	public static function database_last_id()
-	{
+	public static function database_last_id() {
 		return self::database_stage()->adodb5_object()->Insert_ID();
 	}
 
-	public static function database_close( $string_database_route = null )
-	{
+	public static function database_close( $string_database_route = null ) {
 		if( $string_database_route !== null ){
 			self::database_route($string_database_route);
 			self::database_stage()->closeAdodb();
@@ -207,8 +203,7 @@ class ClassDatabase implements InterfaceDatabase
 			self::database_stage(self::database_route());
 		}
 	}
-	private static function database_stage( $string_database_route2close = null )
-	{
+	private static function database_stage( $string_database_route2close = null ) {
 		self::database_session_get();
 		if( !isset( self::$database_stage[self::database_route()] ) ){
 			throw new \Exception( 'Connection not available!<br/>'.self::database_route() );
@@ -219,8 +214,7 @@ class ClassDatabase implements InterfaceDatabase
 		}
 		return self::$database_stage[self::database_route()];
 	}
-	private static function database_mtimeout( $bool_set = true )
-	{
+	private static function database_mtimeout( $bool_set = true ) {
 		if( $bool_set ){
 			self::$database_mtimeout = ini_get('max_execution_time');
 			ini_set('max_execution_time', 0 );
@@ -229,8 +223,7 @@ class ClassDatabase implements InterfaceDatabase
 		}
 	}
 // ---------------------------------------------------------------------------------------
-	public static function database_create_table( $string_table_name, $array_table_fieldset )
-	{
+	public static function database_create_table( $string_table_name, $array_table_fieldset ) {
 		// $array_table_fieldset: Array( Name, Type, Size, Options.. )
 		return self::database_stage()->createTable( $string_table_name, $array_table_fieldset );
 		/*
@@ -275,13 +268,11 @@ class ClassDatabase implements InterfaceDatabase
 						definition.
 		*/
 	}
-	public static function database_drop_table( $string_table_name )
-	{
+	public static function database_drop_table( $string_table_name ) {
 		return self::database_stage()->dropTable( $string_table_name );
 	}
 // ---------------------------------------------------------------------------------------
-	public static function database_recordset( $string_table_name, $string_where_order_by, $bool_resultset = false )
-	{
+	public static function database_recordset( $string_table_name, $string_where_order_by, $bool_resultset = false ) {
 		return self::database_stage()->RecordSet( $string_table_name, $string_where_order_by, $bool_resultset );
 	}
 	/**
@@ -292,8 +283,7 @@ class ClassDatabase implements InterfaceDatabase
 	 * @param bool $bool_delete
 	 * @return bool
 	 */
-	public static function database_record( $string_table_name, $array_fieldset = array(), $array_where = null, $bool_delete = false )
-	{
+	public static function database_record( $string_table_name, $array_fieldset = array(), $array_where = null, $bool_delete = false ) {
 		// TODO: [REMOVE] Unstable Bugfix
 		return self::database_record_bugfix( $string_table_name, $array_fieldset, $array_where, $bool_delete );
 		// TODO: [FIX BUG] In shell.Record oAR->Save on Fieldset DB != Fieldset INSERT (e.g MSSQL NOT NULL)
@@ -307,8 +297,7 @@ class ClassDatabase implements InterfaceDatabase
 	 * @param bool $bool_delete
 	 * @return bool
 	 */
-	private static function database_record_bugfix( $string_table_name, $array_fieldset = array(), $array_where = null, $bool_delete = false )
-	{
+	private static function database_record_bugfix( $string_table_name, $array_fieldset = array(), $array_where = null, $bool_delete = false ) {
 		if( $bool_delete ){
 			if( $array_where !== null ){
 				return self::database_execute( "DELETE FROM ".$string_table_name." WHERE ".implode(' AND ',(array)$array_where) );
@@ -330,8 +319,7 @@ class ClassDatabase implements InterfaceDatabase
 			}
 		}
 	}
-	public static function database_structure( $string_xml_file, $bool_drop = false )
-	{
+	public static function database_structure( $string_xml_file, $bool_drop = false ) {
 		$object_database_definition = Xml::Parser( $string_xml_file )->searchXmlNode('database_definition');
 		$object_database_table_list = $object_database_definition->groupXmlNode( 'database_table' );
 		/** @var \AIOSystem\Core\ClassXmlNode $object_database_table */
@@ -371,7 +359,6 @@ class ClassDatabase implements InterfaceDatabase
 			);
 		}
 	}
-
 	/**
 	 * @static
 	 * @return bool
