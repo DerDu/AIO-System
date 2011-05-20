a3e0aa9eda335d572f70b6991938c079225f30cd
DerDu <blood-demon@gmx.net>
2011-05-20 17:05:56 +0200

    [Code Cleanup]

--- Core/StackObject.php
+++ Core/StackObject.php
@@ -40,8 +40,8 @@
  * @subpackage Stack
  */
 namespace AIOSystem\Core;
-use \AIOSystem\Api\Session;
-use \AIOSystem\Api\Event;
+use \AIOSystem\Api\Stack;
+use \AIOSystem\Module\Cache\Serializer;
 /**
  * @package AIOSystem\Core
  * @subpackage Stack
@@ -49,7 +49,7 @@ use \AIOSystem\Api\Event;
 interface InterfaceStackObject {
 	public static function Instance( $Persistent = true );
 // ---------------------------------------------------------------------------------------
-	public function LoadObject( $Identifier, $Callback = null );
+	public function LoadObject( $Identifier );
 	public function SaveObject( $Identifier, $Object );
 	public function ListObject();
 }
@@ -58,73 +58,23 @@ interface InterfaceStackObject {
  * @subpackage Stack
  */
 class ClassStackObject implements InterfaceStackObject {
-	private $StackRegister = array();
-	private $StackPersistent = true;
-
+	private $StackRegister = null;
 	public static function Instance( $Persistent = true ) {
 		return new ClassStackObject( $Persistent );
 	}
 	private function __construct( $Persistent = true ) {
-		$this->StackPersistent = $Persistent;
+		$this->StackRegister = Stack::Register( $Persistent );
 	}
-
 	public function ListObject() {
-		$this->_load();
-		return array_keys( $this->StackRegister );
+		// TODO: Implement ListObject() method.
 	}
-
-	public function LoadObject( $Identifier, $NameToFileCallback = null ) {
-		$this->_load();
-		$Name = $this->StackRegister[$Identifier][0];
-		$String = $this->StackRegister[$Identifier][1];
-
-		Event::Debug( $this->StackRegister );
-		Event::Debug( $Name );
-		Event::Debug( $String );
-
-		if( !class_exists( $Name ) ) {
-			if( $NameToFileCallback !== null && is_callable( $NameToFileCallback ) ) {
-				$File = $NameToFileCallback( $Name );
-			} else {
-				$File = $Name;
-			}
-			require_once( $File );
-		}
-
-		return $this->_unserialize( $String );
+	public function LoadObject( $Identifier ) {
+		/** @var \AIOSystem\Module\Cache\Serializer $Serializer */
+		$Serializer = $this->StackRegister->getRegister( $Identifier );
+		return $Serializer->Load();
 	}
-
 	public function SaveObject( $Identifier, $Object ) {
-		$this->_load();
-		$Name = get_class( $Object );
-		$String = $this->_serialize( $Object );
-		$this->StackRegister[$Identifier] = array( $Name, $String );
-		$this->_save();
-	}
-
-	private function _serialize( $Object ) {
-		$String = serialize( $Object );
-		$String = base64_encode( $String );
-		return Session::Encode( $String );
-	}
-	private function _unserialize( $String ) {
-		$String = Session::Decode( $String );
-		$String = base64_decode( $String );
-		return unserialize( $String );
-	}
-
-	private function _load() {
-		if( empty( $this->StackRegister ) ) {
-			//Event::Debug( $_SESSION,__METHOD__,__LINE__ );
-			$this->StackRegister = Session::Read( __CLASS__ );
-			//Event::Debug( $this->StackRegister,__METHOD__,__LINE__ );
-		}
-	}
-	private function _save() {
-		if( $this->StackPersistent ) {
-			//Event::Debug( $this->StackRegister,__METHOD__,__LINE__ );
-			Session::Write( __CLASS__, $this->StackRegister );
-			//Event::Debug( $_SESSION,__METHOD__,__LINE__ );
-		}
+		$Serializer = Serializer::Instance( $Object );
+		$this->StackRegister->setRegister( $Identifier, $Serializer );
 	}
 }
